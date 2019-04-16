835630f5ea86d7b947efaca13ec69c87792eafa0
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-04-15 21:36:31 -0400

    completed minimum Requirement

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -34,22 +34,9 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-
       friendsData.push(req.body);
       res.json(true);
-    
 
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
 };
