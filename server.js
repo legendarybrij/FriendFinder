b1fe88931f09b3ac0d6b62462fd93b33b39d2c6c
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-04-18 01:01:33 -0400

    added css

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use('/', express.static('app'));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
@@ -28,6 +28,7 @@ app.use(express.json());
 
  require("./app/routing/apiRoutes")(app);
  require("./app/routing/htmlRoutes")(app);
+ 
 
 // =============================================================================
 // LISTENER
